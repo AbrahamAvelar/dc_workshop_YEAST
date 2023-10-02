0f8dbd2958094be4215283039af114e2502af68a
AbrahamAvelar <jabrahamavelar@gmail.com>
2023-10-02 04:08:50 -0600

    Create GetSamples.sh

---
+++ GetSamples.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+csv_file="/home/javelar/archivos/dc_workshop_YEAST/SampleSheet_dcYEAST2"
+fastq_directory="/mnt/Timina/lmorales/Public/ymez/data/fastq/raw"
+output_directory="/mnt/Timina/lmorales/Public/ymez/tmp/06_genotyping/trees/dc_WorkshopYeast"
+
+while IFS=, read -r prefix _; do
+echo $prefix
+    gunzip -c "${fastq_directory}/${prefix}_R1.fastq.gz" | head -n 400000 > "${output_directory}/${prefix}_R1.fastq"
+    gzip "${output_directory}/${prefix}_R1.fastq"
+
+    gunzip -c "${fastq_directory}/${prefix}_R2.fastq.gz" | head -n 400000 > "${output_directory}/${prefix}_R2.fastq"
+    gzip "${output_directory}/${prefix}_R2.fastq"
+done < "${csv_file}"
+/home/javelar/archivos/dc_workshop_YEAST/GetSamples.sh (END)
+
